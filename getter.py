fd37d0ccb775e6502cd7c709d03ce26317a37115
Georgy <heavypresence@riesup.net>
2022-11-19 10:56:42 +0300

    replaced the recursion with while loop

--- getter.py
+++ getter.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+from datetime import datetime
 from collections import namedtuple
 
 from pprint import pprint
@@ -14,6 +15,17 @@ logger.add(
      'out.log', backtrace=True, diagnose=True, level='DEBUG'
 )
 
+
+def timer_decorator(func):
+
+     def timer():
+         start = datetime.now()
+         logger.info(f'Starting timer at {start}')
+         func()
+         end = datetime.now()
+         logger.success(f'Ending timer at{end}, it took {end-start}.')
+
+     return timer
 
 
 def build_url(logon_data, entity, filters=None):
@@ -48,45 +60,39 @@ def write_contents(entity, contents):
               file.write(',\n')
 
               
-def get_entity(entity, logon_data, tokens_folder, url=None, filters=None, session=None, count=0):
-    if url:
-        # logger.info(f'Getting entities: going through recursion: {count}')
-        r = request_entities(
-             url + filters if filters else url,
-             session=session
-                             )
-    else:
-        logger.info('Getting entities: initiating session')
-        session = build_session(logon_data, tokens_folder)
-        r = request_entities(
-            url=build_url(logon_data, entity, filters if filters else None),
-            session=session
-          )
+def get_entity(entity, logon_data, tokens_folder, filters=None):
+
+    session = build_session(logon_data, tokens_folder) 
+    r = request_entities(
+         url=build_url(logon_data, entity, filters if filter else None),
+         session=session
+    )
 
     write_contents(entity, build_contents(r, entity))
 
-    next = build_next(r)
+    next_url = build_next(r)
 
-    if next:
-        get_entity(
-             entity, logon_data, tokens_folder,
-             next, filters if filters else None,
-             session, count+1
-        )
-    else:
-         logger.success(f'Recursion ended up in {count} depth.')
+    while True:
+        if next_url:
+             r = request_entities(next_url, session)
+             write_contents(entity, build_contents(r, entity))
+             next_url = build_next(r)
+        else:
+            break
 
  
 if __name__ == '__main__':
     url = f'https://{franchize.subdomain}.amocrm.ru/api/v4/events'
-    filters = '?filter[type]=lead_status_changed,lead_added,lead_deleted,lead_restored'
+    filters = '?filter[type]=lead_status_changed,lead_added,lead_deleted,lead_restored&filter[created_at][from]=20220101'
     entity = 'events'
     tokens_folder='tokens/franchize'
 
-    get_entity(entity, franchize, tokens_folder, filters=filters)
+    timer_decorator(get_entity(entity, franchize, tokens_folder, filters=filters))
     # pprint(json.loads(r.text)['_links']['next']['href'])
 
     # with open('events_tmp.json', 'r', encoding='utf-8') as file:
     #     # data = list(json.loads(x) for x in file)
     #     data = json.loads('[' + file.read()[:-2] + ']')
 
+
+    
