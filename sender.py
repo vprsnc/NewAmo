8e5126916ccbe3030a8b901ae617ff97cfcdc45b
Georgy <heavypresence@riesup.net>
2022-11-20 16:59:00 +0300

    lead_status_changes renamed to senders + minor pep8 corr

--- sender.py
+++ sender.py
@@ -5,20 +5,24 @@ from utilities import read_json
 from sqlalchemy.engine import create_engine
 from sqlalchemy.schema import *
 
+from pprint import pprint
 
-engine = create_engine('bigquery://')
+
+# engine = create_engine('bigquery://')
 
 
 def read_entity(entity):
     entity_file = open(f'{entity}_tmp.json', 'r', encoding='utf-8')
     build_entity = 'build_' + entity + '_tuple'
-    return (globals()[build_entity](entry) for entry in tuple(read_json(entity_file)))
+    return (
+        globals()[build_entity](entry) for entry in tuple(read_json(entity_file))
+    )
 
 
 def send_entity(entity):
+    pass
 
 
 if __name__ == "__main__":
-
     read_entity1 = read_entity('lead_status_changes')
-    print(*read_entity1)
+    pprint(tuple(i._asdict() for i in read_entity1))
