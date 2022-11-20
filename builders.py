979251dbe2ffee599589c65d0c4163e60c2f4a31
Georgy <heavypresence@riesup.net>
2022-11-20 16:58:46 +0300

    reading timestamp

--- builders.py
+++ builders.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from entities import *
 
 
@@ -8,7 +9,7 @@ def build_lead_status_changes_tuple(entry):
             entity_id=entry['entity_id'],
             entity_type=entry['entity_type'],
             created_by=entry['created_by'],
-            created_at=entry['created_at'],
+            created_at=str(datetime.fromtimestamp(entry['created_at'])),
             value_after_id=entry['value_after'][0]['lead_status']['id'],
             value_after_pipeline_id=entry['value_after'][0]['lead_status']['pipeline_id'],
             value_before_id=entry['value_before'][0]['lead_status']['id'],
