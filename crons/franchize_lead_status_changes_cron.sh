b16c17822a65c00dd7ad7e6ee7dcab13e98b6d47
Georgy <heavypresence@riesup.net>
2022-12-04 16:50:11 +0300

    cron bash script

---
+++ crons/franchize_lead_status_changes_cron.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mydate=`date`
+cd $HOME/NewAmo/crons
+$HOME/NewAmo/venv/bin/python $HOME/NewAmo/franchize_status_changes_etl.py
+if [ $? -eq 0 ]; then
+    echo "leads cron successful on $mydate" >> $HOME/newAmo.log; else
+    echo "leads cron faield on $mydate" >> $HOME/newAmo.log;
+fi
