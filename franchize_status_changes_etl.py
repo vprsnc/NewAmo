6173120a812f015330e55fcfb89f4a562849f177
Georgy <heavypresence@riesup.net>
2022-12-06 12:04:32 +0300

    checking for code in env

--- franchize_status_changes_etl.py
+++ franchize_status_changes_etl.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
 
     try:
         get_entity(
-            franchize, **arguments,
+            **arguments, logon_data=franchize,
             code=(code if code else None)
         )
 
