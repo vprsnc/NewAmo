c1400bd44ec528c94d0b13cde093ac207c84574f
Georgy <heavypresence@riesup.net>
2022-12-06 12:44:45 +0300

    correct naming

--- crons/cron_franchize_lead_status_changes.sh
+++ crons/cron_franchize_lead_status_changes.sh
@@ -2,7 +2,7 @@
 
 mydate=`date`
 cd $HOME/NewAmo
-$HOME/NewAmo/venv/bin/python $HOME/NewAmo/etl_franchize_status_changes.py
+$HOME/NewAmo/venv/bin/python $HOME/NewAmo/etl_franchize_lead_status_changes.py
 if [ $? -eq 0 ]; then
     echo "leads status changes cron successful on $mydate" >> $HOME/newAmo.log; else
     echo "leads status change scron failed on $mydate" >> $HOME/newAmo.log;
