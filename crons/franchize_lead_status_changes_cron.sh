a084d742813d2af75ab9b984e3e7c11ca514ad8a
Georgy <heavypresence@riesup.net>
2022-12-06 12:38:41 +0300

    fix naming

--- crons/franchize_lead_status_changes_cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mydate=`date`
-cd $HOME/NewAmo
-$HOME/NewAmo/venv/bin/python $HOME/NewAmo/franchize_status_changes_etl.py
-if [ $? -eq 0 ]; then
-    echo "leads cron successful on $mydate" >> $HOME/newAmo.log; else
-    echo "leads cron faield on $mydate" >> $HOME/newAmo.log;
-fi
---
+++ etl_franchize_lead_status_changes.py
@@ -0,0 +1,54 @@
+import os
+
+from datetime import datetime
+from dateutil import parser
+
+from loguru import logger
+from amo.getter import get_entity
+
+from setup import franchize
+from sender import send_entity
+
+logger.add(
+     'logs/franchize_lead_status_changes.log', backtrace=True, diagnose=True, level='DEBUG'
+)
+
+with open('franchize_lead_status_changes_last_date.txt', 'r') as f:
+    last_date = str(
+        datetime.timestamp(parser.parse(f.read()))
+    ).split('.', maxsplit=1)[0]
+
+try:
+    code = os.environ["CODE"]
+except KeyError:
+    code = None
+
+arguments = {
+    'entity': "lead_status_changes",
+    'amo':  'franchize',
+    'filters': f'?filter[type]=lead_status_changed&filter[created_at][from]={last_date}'
+    }
+
+
+if __name__ == "__main__":
+
+    logger.info(
+        f"Starting {arguments['entity']} ETL process at {datetime.now()}"
+    )
+
+    try:
+        get_entity(
+            **arguments, logon_data=franchize,
+            code=(code if code else None)
+        )
+
+        try:
+            send_entity(arguments['entity'], 'franchize', if_exists='append')
+            logger.success("ETL process finished successfully, cleaning up...")
+            open(f"temp_data/{arguments['entity']}_tmp.json", "w").close()
+
+        except Exception as e:
+            logger.critical(f"Sedning process failure: {e}")
+
+    except Exception as e:
+        logger.critical(f"ETL process failure: {e}")
